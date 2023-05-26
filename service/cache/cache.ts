271bf0778dea59afcc41dcceb9204cd64271917b
Jennifer Lyden <jennifer.lyden@powerschool.com>
2023-05-25 20:54:52 -0400

    add cache as quick and dirty datastore

---
+++ service/cache/cache.ts
@@ -0,0 +1,14 @@
+import NodeCache from "node-cache";
+
+const CACHE_TTL = 36000; // 60 * 60 * 10
+
+const cache = new NodeCache({ stdTTL: CACHE_TTL });
+
+export function saveToCache(key: string, value: object): void {
+  cache.set(key, value);
+}
+
+export function getFromCache(key: string): object {
+  const value = cache.get(key);
+  return value as object;
+}
